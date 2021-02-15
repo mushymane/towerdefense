6e7ca22ee7c3e64d5e8a281f094b14f943cd3d22
Luigi Lantin <mushymanee@gmail.com>
2021-02-15 02:43:48 -0800

    added obstacle for attackers and health on defenders

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class Attacker : MonoBehaviour
 {
     [Range(0f, 5)] float currentSpeed = 1f;
+    GameObject currentTarget;
 
     
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
     }
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("isAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        if (!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if (health)
+        {
+            health.DealDamage(damage);
+        }
+    }
+
+    private void UpdateAnimationState()
+    {
+        if (!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("isAttacking", false);
+        }
     }
 }
