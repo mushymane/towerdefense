65cb70f94269ee669e9832d388a7210d2db3f2d8
Luigi Lantin <mushymanee@gmail.com>
2021-02-15 15:38:45 -0800

    new attacker prefab with jump animation, lives, lose screen

---
+++ Glitch Garden/Assets/Scripts/LivesDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesDisplay : MonoBehaviour
+{
+    [SerializeField] int lives = 5;
+    [SerializeField] int damage = 1;
+    Text livesText;
+
+    void Start()
+    {
+        livesText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    // Update is called once per frame
+    void UpdateDisplay()
+    {
+        livesText.text = lives.ToString();
+    }
+
+    public void TakeLife()
+    {
+        lives -= damage;
+        UpdateDisplay();
+
+        if (lives <= 0)
+        {
+            FindObjectOfType<LevelLoader>().LoadYouLose();
+        }
+    }
+}
