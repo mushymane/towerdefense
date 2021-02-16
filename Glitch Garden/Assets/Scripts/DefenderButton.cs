44aced6ef871f08301f2058bae8f7373e01f1165
Luigi Lantin <mushymanee@gmail.com>
2021-02-16 00:36:46 -0800

    core gameplay finished, level progression next

--- Glitch Garden/Assets/Scripts/DefenderButton.cs
+++ Glitch Garden/Assets/Scripts/DefenderButton.cs
@@ -1,10 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] Defender defenderPrefab;
+
+    private void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if (!costText)
+        {
+            Debug.LogWarning(name + " has no cost text, maybe add some!");
+        }
+        else
+        {
+            costText.text = defenderPrefab.GetStarCost().ToString();
+        }
+    }
 
     private void OnMouseDown()
     {
